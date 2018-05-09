749bca6779fd7d255a9c2f26f92f5ef45b6c232f
Claudio Costa <claudiocbamt@gmail.com>
2018-05-09 08:09:30 -0400

    criação da classe categoria

--- src/main/java/com/claudiocosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/claudiocosta/cursomc/resources/CategoriaResource.java
@@ -1,18 +1,33 @@
 package com.claudiocosta.cursomc.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.claudiocosta.cursomc.domain.Categoria;
+
+@Controller
 @RestController
 @RequestMapping(value="categorias")
 
 public class CategoriaResource {
 	
 	@RequestMapping(method= RequestMethod.GET) 
-	public String Listar()
+	public List<Categoria> Listar()
 	{
-		return "rest esta funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+				
+				List<Categoria> lista = new ArrayList<>();
+						lista.add(cat1);
+				        lista.add(cat2);
+				
+		
+		return lista;
 		
 	}
 	
