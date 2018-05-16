2da95ec756af6773638dc95edce3ac33e5cd7a5c
Claudio Costa <claudiocbamt@gmail.com>
2018-05-16 11:59:07 -0400

    Personalizando o retorno da valiadação

---
+++ java/com/claudiocosta/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,32 @@
+package com.claudiocosta.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+
+	public void AddError(String fieldName, String message ) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	
+
+}
--- java/com/claudiocosta/cursomc/services/CategoriaService.java
+++ java/com/claudiocosta/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.claudiocosta.cursomc.domain.Categoria;
+import com.claudiocosta.cursomc.dto.CategoriaDTO;
 import com.claudiocosta.cursomc.repositories.CategoriaRepository;
 import com.claudiocosta.cursomc.services.exception.DataIntegrityException;
 import com.claudiocosta.cursomc.services.exception.ObjectNotFoundException;
@@ -63,6 +64,11 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(Page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO)
+	{
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
 
