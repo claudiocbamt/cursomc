3333ab8c79c740a740b60c3ce4a8b160dfab713c
Claudio Costa <claudiocbamt@gmail.com>
2018-05-27 21:14:25 -0400

    Configuração e envio do prjeto ara produção

--- java/com/claudiocosta/cursomc/config/DevConfig.java
+++ java/com/claudiocosta/cursomc/config/DevConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.claudiocosta.cursomc.services.DBService;
 
 @Configuration
-@Profile("dev")
+@Profile("prod")
 
 public class DevConfig {
 
